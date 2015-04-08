3bc4f225cc60104f0b02284bfd4f9e49df996f56
giocatori86 <skk420@few.vu.nl>
2015-04-08 11:07:39 +0200

    back change

--- movieHandling.js
+++ movieHandling.js
@@ -5,11 +5,48 @@ var titleBook = "";
 var movieName="Reservoir Dogs";
 var moviename ="";
 var movieTitle=[];
+var XML;
+function xmlToJson(xml) {
+	
+	// Create the return object
+	var obj = {};
 
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
 
 function fetchMovieRecommendations(callback) {
     var baseUrl = "http://www.tastekid.com/api/";
-    var type ="movie";
+    var type ="movies";
     var key = "131520-ReadAMov-GLLWO1NB";
 
     var url = baseUrl + "/similar?q=";
@@ -49,39 +86,34 @@ var url = baseUrl+"/?t="+encodeURIComponent(currentTitle)+"&type=movie&r=json&to
 /* Books below this line -JK */
 function fetchBookRecommendations(callback) {
 
-var baseUrl = "http://www.tastekid.com/api/"
-			var type 	=	"book";
-			var key = "131520-ReadAMov-8Z16J8NJ";
-		
-			/ read in array /
+    var baseUrl = "http://www.tastekid.com/api/";
+    var type ="books";
+    var key = "131520-ReadAMov-GLLWO1NB";
 
-			var url = baseUrl + "/similar?q=";
-			url += encodeURIComponent(query)+"&type="+type+"&k="+key;
-     return $.ajax({
-        dataType: 'jsonp',
-		url: url,
-		async: false
-    })
-    .done(callback)
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        // Handle error
+    var url = baseUrl + "/similar?q=";
+    url += encodeURIComponent(query)+"&type="+type+"&k="+key;
+        return $.ajax({
+            dataType: 'jsonp',
+    url: url,
+    async: false
+        })
+        .done(callback)
+        .fail(function(jqXHR, textStatus, errorThrown) {
     });
-}
+};
 
 function fetchBookInformation(callback) {
-var baseUrl = "https://www.goodreads.com/book/"
-var titleBook = "taken"
+var baseUrl = "https://www.goodreads.com/search/"
 var key = "wCFJJNFqLT034b7Nqw0duQ";
-var url = baseUrl+"title.json?&title="+titleBook +"key="+key;
-console.log(url);
-
-        return $.ajax({
-        dataType: 'jsonp',
-		url: url,
-		async: false
-    })
-    .done(callback)
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        // Handle error
-    });
-}
+var url = baseUrl+"index.xml?&key="+key+"&q="+encodeURIComponent(currentTitle);
+console.log(url);        
+        $.get("http://query.yahooapis.com/v1/public/yql",
+    {
+        q: "select * from xml where url=\""+url+"\"",
+        format: "xml"
+    },
+    callback
+   );
+ }
+    
+   
--- output.js
+++ output.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    fetchBookRecommendations(function(da) {
+        bookArray = [];
+        var x2js = new X2JS();
+         for(index = 0; index < 6; index++) {
+            currentTitle = da["Similar"].Results[index].Name;
+            fetchBookInformation(function(da) {
+            var jsonObj = x2js.xml2json(da);
+            bookArray.push(jsonObj);
+           
+            });
+        }
+        
 
     fetchMovieRecommendations(function(data) {
         movieArray = [];
@@ -8,120 +20,128 @@ $(document).ready(function () {
                 movieArray.push(d);
             });
         }
-        buildTemplate(movieArray);
+        buildTemplate(movieArray,bookArray);
+        console.log(bookArray);
     });
 
-    function buildTemplate(movieArray){
+});
+
+    function buildTemplate(movieArray,bookArray){
         var output = $("#output");    
         var template = $("#test2").html();
-    
-        fetchMovieRecommendations(function(data) {
-        
+
+
             var data = {
             books: [
-            {   nameOfBook_home:data.Plot,
+            {   nameOfBook_home:"Christophe",
                 nameOfBook_holiday: "Christophe",
                 nameOfBook_adventure: "Christophe",
                 nameOfBook_advance: "Christophe",
                 nameOfBook_crime: "Christophe",
                 nameOfBook_history: "Christophe",
 
-                explanationOfBook_home: data.Plot,
+                explanationOfBook_home:"Christophe",
                 explanationOfBook_holiday: "Christophe",
                 explanationOfBook_adventure: "Christophe",
                 explanationOfBook_advance: "Christophe",
                 explanationOfBook_crime: "Christophe",
                 explanationOfBook_history: "Christophe",
 
-                authorOfBook_home: data.Director,
+                authorOfBook_home: "Christophe",
                 authorOfBook_holiday: "Christophe",
                 authorOfBook_adventure: "Christophe",
                 authorOfBook_advance: "Christophe",
                 authorOfBook_crime: "Christophe",
                 authorOfBook_history: "Christophe",
 
-                DateOfBook_home: data.Released,
+                DateOfBook_home: "Christophe",
                	DateOfBook_holiday: "Christophe",
                 DateOfBook_adventure: "Christophe",
                 DateOfBook_advance: "Christophe",
                 DateOfBook_crime: "Christophe",
                 DateOfBook_history: "Christophe",
 
-                themeOfBook_home: data.Genre,
+                themeOfBook_home: "Christophe",
                	themeOfBook_holiday: "Christophe",
                 themeOfBook_adventure: "Christophe",
                 themeOfBook_advance: "Christophe",
                 themeOfBook_crime: "Christophe",
                 themeOfBook_history: "Christophe",
 
-                ratingOfBook_home: data.imdbRating,
+                ratingOfBook_home: "Christophe",
                	ratingOfBook_holiday: "Christophe",
                 ratingOfBook_adventure: "Christophe",
                 ratingOfBook_advance: "Christophe",
                 ratingOfBook_crime: "Christophe",
                 ratingOfBook_history: "Christophe",
 
-                posterOfBook_home: data.Poster,
+                posterOfBook_home: "Christophe",
                 posterOfBook_holiday: "Christophe",
                 posterOfBook_adventure: "Christophe",
                 posterOfBook_advance: "Christophe",
                 posterOfBook_crime: "Christophe",
                 posterOfBook_history: "Christophe",
 
-                nameOfMovie_home:data.Plot,
-                nameOfMovie_holiday: "Christophe",
-                nameOfMovie_adventure: "Christophe",
-                nameOfMovie_advance: "Christophe",
-                nameOfMovie_crime: "Christophe",
-                nameOfMovie_history: "Christophe",
+                nameOfMovie_home:movieArray[0].Title,
+                nameOfMovie_holiday: movieArray[1].Title,
+                nameOfMovie_adventure: movieArray[2].Title,
+                nameOfMovie_advance: movieArray[3].Title,
+                nameOfMovie_crime: movieArray[4].Title,
+                nameOfMovie_history: movieArray[5].Title,
 
-                explanationOfMovie_home: data.Plot,
-                explanationOfMovie_holiday: "Christophe",
-                explanationOfMovie_adventure: "Christophe",
-                explanationOfMovie_advance: "Christophe",
-                explanationOfMovie_crime: "Christophe",
-                explanationOfMovie_history: "Christophe",
+                explanationOfMovie_home: movieArray[0].Plot,
+                explanationOfMovie_holiday: movieArray[1].Plot,
+                explanationOfMovie_adventure: movieArray[2].Plot,
+                explanationOfMovie_advance: movieArray[3].Plot,
+                explanationOfMovie_crime: movieArray[4].Plot,
+                explanationOfMovie_history: movieArray[5].Plot,
 
-                authorOfMovie_home: data.Director,
-                authorOfMovie_holiday: "Christophe",
-                authorOfMovie_adventure: "Christophe",
-                authorOfMovie_advance: "Christophe",
-                authorOfMovie_crime: "Christophe",
-                authorOfMovie_history: "Christophe",
+                authorOfMovie_home: movieArray[0].Writer,
+                authorOfMovie_holiday: movieArray[1].Writer,
+                authorOfMovie_adventure: movieArray[2].Writer,
+                authorOfMovie_advance: movieArray[3].Writer,
+                authorOfMovie_crime: movieArray[4].Writer,
+                authorOfMovie_history: movieArray[5].Writer,
 
-                DateOfMovie_home: data.Released,
-                DateOfMovie_holiday: "Christophe",
-                DateOfMovie_adventure: "Christophe",
-                DateOfMovie_advance: "Christophe",
-                DateOfMovie_crime: "Christophe",
-                DateOfMovie_history: "Christophe",
+                DateOfMovie_home: movieArray[0].Year,
+                DateOfMovie_holiday: movieArray[1].Year,
+                DateOfMovie_adventure: movieArray[2].Year,
+                DateOfMovie_advance: movieArray[3].Year,
+                DateOfMovie_crime: movieArray[4].Year,
+                DateOfMovie_history: movieArray[5].Year,
 
-                themeOfMovie_home: data.Genre,
-                themeOfMovie_holiday: "Christophe",
-                themeOfMovie_adventure: "Christophe",
-                themeOfMovie_advance: "Christophe",
-                themeOfMovie_crime: "Christophe",
-                themeOfMovie_history: "Christophe",
+                themeOfMovie_home: movieArray[0].Genre,
+                themeOfMovie_holiday: movieArray[1].Genre,
+                themeOfMovie_adventure: movieArray[2].Genre,
+                themeOfMovie_advance: movieArray[3].Genre,
+                themeOfMovie_crime: movieArray[4].Genre,
+                themeOfMovie_history: movieArray[5].Genre,
 
-                ratingOfMovie_home: data.imdbRating,
-                ratingOfMovie_holiday: "Christophe",
-                ratingOfMovie_adventure: "Christophe",
-                ratingOfMovie_advance: "Christophe",
-                ratingOfMovie_crime: "Christophe",
-                ratingOfMovie_history: "Christophe",
+                ratingOfMovie_home: movieArray[0].imdbRating,
+                ratingOfMovie_holiday: movieArray[1].imdbRating,
+                ratingOfMovie_adventure: movieArray[2].imdbRating,
+                ratingOfMovie_advance: movieArray[3].imdbRating,
+                ratingOfMovie_crime: movieArray[4].imdbRating,
+                ratingOfMovie_history: movieArray[5].imdbRating,
 
-                posterOfMovie_home: data.Poster,
-                posterOfMovie_holiday: "Christophe",
-                posterOfMovie_adventure: "Christophe",
-                posterOfMovie_advance: "Christophe",
-                posterOfMovie_crime: "Christophe",
-                posterOf_history: "Christophe",
+                posterOfMovie_home: movieArray[0].Poster,
+                posterOfMovie_holiday: movieArray[1].Poster,
+                posterOfMovie_adventure: movieArray[2].Poster,
+                posterOfMovie_advance: movieArray[3].Poster,
+                posterOfMovie_crime: movieArray[4].Poster,
+                posterOfMovie_history: movieArray[5].Poster,
+
+                ratingOfMovie_home: movieArray[0].imdbRating,
+                ratingOfMovie_holiday: movieArray[1].imdbRating,
+                ratingOfMovie_adventure: movieArray[2].imdbRating,
+                ratingOfMovie_advance: movieArray[3].imdbRating,
+                ratingOfMovie_crime: movieArray[4].imdbRating,
+                ratingrOfMovie_history: movieArray[5].imdbRating,
             }
             ]};
-            console.log(movieArray); // Nu prima usable -JK
 
         html = Mustache.render(template, data);
         output.append(html);
-        });
+
     }
 });
